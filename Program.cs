60b164d69347d6fb257da923fad8f90e4aae5d93
Enrique Lovera <kikelovera7@gmail.com>
2022-06-06 21:30:16 -0300

    program class with main method ? new net6 template hide main method

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿//WE ARE GOING TO PERFORM SOME APIS GET CALL AND DO SOME DATA PROCESSING. USING .NET6 NEW VERSION.
+using RequestTask;
+
+
+
+
+var repositories= await RequestMethod.ProcessRepositories();
+
+foreach (var repo in repositories){
+    Console.WriteLine(repo.Name);
+    Console.WriteLine(repo.Description);
+    Console.WriteLine(repo.GitHubHomeUrl);
+    Console.WriteLine(repo.Homepage);
+    Console.WriteLine(repo.Watchers);
+    Console.WriteLine();
+}
+
+
+
+
